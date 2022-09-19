56f484ad8a23da9196956a3aac22e30004c181e8
damianrobert <damianrobert666@yahoo.com>
2022-09-19 13:03:18 +0300

    am pus rockPapperScissors

---
+++ script.js
@@ -0,0 +1,52 @@
+const buttons = document.querySelectorAll('button')
+const display = document.querySelector('.display-result')
+const showComputerChoice = document.getElementById('show-computer-choice')
+const showUserChoice = document.getElementById('show-user-choice')
+let userChoice
+
+
+function getRandomChoice() {
+  let randomNumber = Math.floor(Math.random() * 3)
+
+  switch(randomNumber){
+    case 0 : 
+        return "rock"
+
+    case 1 :
+        return "papper"
+    
+    case 2 :
+        return "scissors"    
+  }
+}
+
+
+
+buttons.forEach(function (button){
+    button.addEventListener('click', (e) => {
+        userChoice = e.target.className
+        const computerChoice = getRandomChoice()
+
+    if(userChoice === computerChoice){
+        display.innerHTML = "It' s a tie!"
+    } else if (userChoice === 'papper' && computerChoice === 'rock'){
+        display.innerHTML = "Win!"
+    } else if (userChoice === 'papper' && computerChoice === 'scissors'){
+        display.innerHTML = "Lose!"
+    } else if (userChoice === 'rock' && computerChoice === 'papper'){
+        display.innerHTML = "Lose!"
+    } else if (userChoice === 'rock' && computerChoice === 'scissors'){
+        display.innerHTML = "Win!"
+    } else if (userChoice === 'scissors' && computerChoice === 'papper'){
+        display.innerHTML = "Win!"
+    } else if (userChoice === 'scissors' && computerChoice === 'rock'){
+        display.innerHTML = "Lose!"
+    }
+
+    showUserChoice.innerHTML = "USER CHOICE: " + userChoice
+    showComputerChoice.innerHTML = "COMPUTER CHOICE: " + computerChoice
+
+  })
+           
+})
+
